77e5d7b30f5d3b7c36a23d2a412ca186973eef6b
Thanadetch Lertwuthikarn <tlertwuthikarn@palo-it.com>
2023-12-26 21:57:55 +0700

    update provider

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,30 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  }
 ];
